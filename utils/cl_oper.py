9b78804e2b613179832a026921181814db62709d
Nikolai <kolamba.r@gmail.com>
2023-05-09 18:17:21 +0300

    class Operation have been made

--- utils/cl_oper.py
+++ utils/cl_oper.py
@@ -1,2 +1,10 @@
 class Operation:
-    ...
+
+    def __init__(self):
+        self.id = None
+        self.date = None
+        self.state = bool
+        self.operationAmount = None
+        self.description = None
+        self._from = None
+        self._to = None
