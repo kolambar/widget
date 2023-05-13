fba4c53633aae77388f6c8a8e9a835172a7512b0
Nikolai <kolamba.r@gmail.com>
2023-05-13 20:13:05 +0300

    add two case of function with incorrect data

--- tests/tets_function.py
+++ tests/tets_function.py
@@ -1,5 +1,13 @@
 from utils.function import from_json_to_data
 import os
+import pytest
 
 def test_from_json_to_data():
     assert from_json_to_data(os.path.join("test_data", "good.json")) == []
+
+
+def test_with_mistake_from_json_to_data():
+    with pytest.raises(NameError):
+        from_json_to_data(incorrect_path_to_json)
+    with pytest.raises(FileNotFoundError):
+        from_json_to_data("incorrect_path_to_json")
