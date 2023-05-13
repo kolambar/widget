5cb39d204c7c5a4568dd1980a5de801f729e5564
Nikolai <kolamba.r@gmail.com>
2023-05-14 02:30:35 +0300

    add json and file for class test, force to work generator of elements. there also add path to file and function

---
+++ tests/test_cl_oper.py
@@ -0,0 +1,4 @@
+from utils import cl_oper
+
+def test_born_from_json():
+    pass
--- utils/cl_oper.py
+++ utils/cl_oper.py
@@ -1,10 +1,21 @@
+import function
+import os
 class Operation:
 
-    def __init__(self):
-        self.id = None
-        self.date = None
-        self.state = bool
-        self.operationAmount = None
-        self.description = None
-        self._from = None
-        self._to = None
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def __repr__(self):
+        return f'{self.id}, {self.state}, {self.description}, {self.operationAmount}'
+
+    def born_from_json(data):
+        list_of_operation = []
+        for diction in data:
+            if not diction:
+                continue
+            operation = Operation(**diction)
+            list_of_operation.append(operation)
+        return list_of_operation
+
+data = function.from_json_to_data(os.path.join("..", 'operations.json'))
