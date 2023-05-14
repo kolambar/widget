5f0a0846a499b924ab7b654f08cecc46d5bfaec4
Nikolai <kolamba.r@gmail.com>
2023-05-14 20:50:33 +0300

    fix problem with from

--- utils/cl_oper.py
+++ utils/cl_oper.py
@@ -4,11 +4,16 @@ class Operation:
 
     def __init__(self, **kwargs):
         for key, value in kwargs.items():
-            setattr(self, key, value)
+            if key == "from":
+                setattr(self, "sender", value)
+            else:
+                setattr(self, key, value)
+            self.sender = getattr(self, "sender", "Счета отправителя нет. Это операция по открытию вклада.")
 
     def __repr__(self):
-        return f'{self.id}, {self.state}, {self.description}, {self.operationAmount}'
+        return f'id: {self.id}, state: {self.state}, date: {self.date}, operationAmount: {self.operationAmount}, description: {self.description}, from: {self.sender}, to: {self.to}'
 
+    # @staticmethod
     def born_from_json(data):
         list_of_operation = []
         for diction in data:
@@ -19,3 +24,5 @@ class Operation:
         return list_of_operation
 
 data = function.from_json_to_data(os.path.join("..", 'operations.json'))
+
+print(Operation.born_from_json(data))
